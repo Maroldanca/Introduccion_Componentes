2f38712c5d3a6713749f8eda686eb0de85618c9b
Miguel Roldan <miguelangelroldan09@gmail.com>
2024-08-13 20:15:46 -0500

    Se crea primer componente para estructurar el proyecto

---
+++ src/Contenido.jsx
@@ -0,0 +1,11 @@
+function Contenido(){
+
+    return(
+        <main>
+            <h1>Hola mundo!!!</h1>
+            <p>hola holaaaa</p>
+        </main>
+    )
+}
+
+export default Contenido
---
+++ src/assets/Input.jsx
@@ -0,0 +1,10 @@
+function Input(){
+    return(
+        <main>
+            <h1>Titulo del lorem</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aliquid praesentium ducimus dolorum impedit, laboriosam tenetur earum animi. Nobis exercitationem id reiciendis quam eius adipisci atque accusantium repellendus earum commodi.</p>
+        </main>
+    )
+}
+
+export default Input
