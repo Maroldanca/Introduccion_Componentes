8d94cc81285a8d694e6ce367959be0dc534e31a6
Miguel Roldan <miguelangelroldan09@gmail.com>
2024-08-20 19:17:00 -0500

    Cambitos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Contenido from './Contenido'
-import Input from './Input'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
