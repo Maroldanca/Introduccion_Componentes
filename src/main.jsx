65ab12713b6ee69d70bea263c07961a9662f0d4a
Miguel Roldan <miguelangelroldan09@gmail.com>
2024-08-20 19:08:20 -0500

    Cambios2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,39 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Contenido />
-    <Input />
+    <header className='encabezado'>
+      <section className='logotipo'></section>
+      <nav className='menu'>
+        <a href="Enlace"></a>
+        <a href="Enlace"></a>
+        <a href="Enlace"></a>
+        <a href="Enlace"></a>
+        <a href="Enlace"></a>
+      </nav>
+    </header>
+    <div>
+      <main>
+        <h1>Galeria de imagenes</h1>
+        <div>
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+          <img src="" alt="img" />
+        </div>
+      </main>
+      <div>
+        <section>
+          <h2>Titulo de contenido</h2>
+        </section>
+        <section>
+          <h2>Titulo de contenido</h2>
+        </section>
+      </div>
+    </div>
   </StrictMode>,
 )
