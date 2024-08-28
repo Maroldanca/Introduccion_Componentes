68f59533c91411f197521278537af4366fb3591a
Miguel Roldan <miguelangelroldan09@gmail.com>
2024-08-27 20:49:25 -0500

    NuevoRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import Encabezado from './Encabezado'
 import Contenido from './Contenido'
 import './index.css'
+import Home from './pages/home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Home />
     <Encabezado />
     <div className='contenido'>
       <Contenido />
